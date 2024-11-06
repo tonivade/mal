c12ead70da5d14a006fc94a13bdb9cad990544ec
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-11-06 20:46:49 +0100

    generate docker image for testing

--- impls/java23/src/main/java/mal/App.java
+++ impls/java23/src/main/java/mal/App.java
@@ -8,10 +8,10 @@ public class App {
       throw new IllegalArgumentException("mandatory step");
     }
     switch (args[0]) {
-      case "step0" -> {
+      case "step0_repl" -> {
         step0_repl.main(Arrays.copyOfRange(args, 1, args.length));
       }
-      case "step1" -> {
+      case "step1_read_print" -> {
         step1_read_print.main(Arrays.copyOfRange(args, 1, args.length));
       }
       default -> throw new IllegalArgumentException("invalid step: " + args[0]);
--- impls/java23/src/main/java/mal/Printer.java
+++ impls/java23/src/main/java/mal/Printer.java
@@ -2,14 +2,21 @@ package mal;
 
 import java.util.stream.Collectors;
 
+import org.apache.commons.text.StringEscapeUtils;
+
 public class Printer {
 
   public static String print(Mal val) {
     return switch (val) {
+      case Mal.MalConstant(var name) -> name;
       case Mal.MalSymbol(var name) -> name;
+      case Mal.MalString(var value) -> "\"" + StringEscapeUtils.escapeJava(value) + "\"";
       case Mal.MalNumber(var value) -> Integer.toString(value);
       case Mal.MalList(var list) -> {
         yield list.stream().map(Printer::print).collect(Collectors.joining(" ", "(", ")"));
+      }
+      case Mal.MalVector(var list) -> {
+        yield list.stream().map(Printer::print).collect(Collectors.joining(" ", "[", "]"));
       }
       default -> null;
     };
