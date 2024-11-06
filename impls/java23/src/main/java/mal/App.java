414cfd789386b6b668b32f871e4f39c715feeb9a
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-11-06 13:01:44 +0100

    step0

--- impls/java23/src/main/java/mal/App.java
+++ impls/java23/src/main/java/mal/App.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("hello " + Arrays.toString(args));
+    if (args.length == 0) {
+      throw new IllegalArgumentException("mandatory step");
+    }
+    switch (args[0]) {
+      case "step0_repl" -> {
+        step0_repl.main(Arrays.copyOfRange(args, 1, args.length));
+      }
+      default -> throw new IllegalArgumentException("invalid step: " + args[0]);
+    }
   }
 }
