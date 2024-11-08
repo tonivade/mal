4814f285a657b6c4459a0902498221fc45acd332
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-11-08 12:07:44 +0100

    fix step 2

--- impls/java23/src/main/java/mal/Evaluator.java
+++ impls/java23/src/main/java/mal/Evaluator.java
@@ -2,13 +2,18 @@ package mal;
 
 import static mal.Mal.ZERO;
 import static mal.Mal.list;
+import static mal.Mal.map;
+import static mal.Mal.vector;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import mal.Mal.MalFunction;
 import mal.Mal.MalList;
+import mal.Mal.MalMap;
 import mal.Mal.MalNumber;
 import mal.Mal.MalSymbol;
+import mal.Mal.MalVector;
 
 public class Evaluator {
 
@@ -21,6 +26,7 @@ public class Evaluator {
 
   static Mal eval(Mal ast) {
     return switch (ast) {
+
       case MalSymbol(var name) -> {
         var function = ENV.get(name);
         if (function == null) {
@@ -28,6 +34,7 @@ public class Evaluator {
         }
         yield function;
       }
+
       case MalList(var values) when !values.isEmpty() -> {
         var evaluated = values.stream().map(Evaluator::eval).toList();
         yield switch (evaluated.getFirst()) {
@@ -35,6 +42,18 @@ public class Evaluator {
           default -> throw new UnsupportedOperationException();
         };
       }
+
+      case MalVector(var values) when !values.isEmpty() -> {
+        var evaluated = values.stream().map(Evaluator::eval).toList();
+        yield vector(evaluated);
+      }
+
+      case MalMap(var map) when !map.isEmpty() -> {
+        var evaluated = map.entrySet().stream()
+          .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, entry -> eval(entry.getValue())));
+        yield map(evaluated);
+      }
+
       default -> ast;
     };
   }
