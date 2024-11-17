3f9a919ea3a3043fca2e0af0b54fa093bd4b8c1c
Antonio Muñoz <antoniogmc@gmail.com>
2024-11-17 16:08:43 +0100

    fix step 9

--- impls/java23/src/main/java/mal/MalException.java
+++ impls/java23/src/main/java/mal/MalException.java
@@ -1,14 +1,28 @@
 package mal;
 
+import static mal.MalNode.string;
 import static mal.Printer.print;
+
+import java.util.Objects;
 
 public class MalException extends RuntimeException {
 
-  public MalException(MalNode value) {
-    this(print(value, false));
+  private final MalNode node;
+
+  public MalException(MalNode node) {
+    this.node = Objects.requireNonNull(node);
   }
 
   public MalException(String message) {
-    super(message);
+    this(string(message));
+  }
+
+  @Override
+  public String getMessage() {
+    return getMessage(false);
+  }
+
+  public String getMessage(boolean pretty) {
+    return print(node, pretty);
   }
 }
