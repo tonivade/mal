b298d4f99746ee8413f3382a46e79de02411826a
Antonio Muñoz <antoniogmc@gmail.com>
2024-11-30 16:56:26 +0100

    add history, autocomplete

--- impls/java23/src/main/java/mal/Readline.java
+++ impls/java23/src/main/java/mal/Readline.java
@@ -1,13 +1,43 @@
 package mal;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.DefaultHighlighter;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.reader.impl.completer.StringsCompleter;
+import org.jline.reader.impl.history.DefaultHistory;
 
 public class Readline {
     
   private static final LineReader reader = LineReaderBuilder.builder()
+    .variable(LineReader.HISTORY_FILE, System.getProperty("user.home") + "/.malhistory")
+    .variable(LineReader.HISTORY_FILE_SIZE, 1000)
     .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
+    .history(new DefaultHistory())
+    .completer(new StringsCompleter("println", "prn", "list", "list?", "empty?", 
+      "count", "pr-str", "str", "read-string", "slurp", "atom", "atom?", "deref", "reset!", 
+      "swap!", "cons", "concat", "vec", "macro?", "nth", "first", "rest", "throw", "apply", 
+      "map", "nil", "nil?", "true", "true?", "false", "false?", "symbol?", "keyword?", 
+      "vector?", "sequential?", "map?", "contains?", "symbol", "keyword", "vector", "get",
+      "keys", "vals", "hash-map", "assoc", "dissoc", "readline", "time-ms", "meta", "with-meta", 
+      "fn?", "string?", "number?", "seq", "conj", "def!", "defmacro!", "let*", "do", "try*",
+      "if", "fn", "quote", "quasiquote", "catch*"))
+    .highlighter(new DefaultHighlighter())
+    .parser(new DefaultParser().lineCommentDelims(new String[] { ";" }))
     .build();
+
+  static {
+    Runtime.getRuntime().addShutdownHook(Thread.ofPlatform().unstarted(() -> {
+      try {
+        reader.getHistory().save();
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }));
+  }
 
   public static String readline(String prompt) {
     return reader.readLine(prompt);
