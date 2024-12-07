b952531ceed06cf9c8efd311dd6d1034b3015e95
Antonio Muñoz <antoniogmc@gmail.com>
2024-12-08 00:36:02 +0100

    add method find-matches

--- impls/java23/src/main/java/mal/lib/Strings.java
+++ impls/java23/src/main/java/mal/lib/Strings.java
@@ -5,12 +5,16 @@
 package mal.lib;
 
 import static java.util.Map.entry;
+import static mal.MalNode.NIL;
 import static mal.MalNode.function;
 import static mal.MalNode.list;
 import static mal.MalNode.number;
+import static mal.MalNode.string;
 import static mal.Trampoline.done;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import mal.MalNode;
@@ -31,6 +35,29 @@ public interface Strings {
     return done(list(Stream.of(split).map(MalNode::string)));
   };
 
+  MalLambda FIND_MATCHES = args -> {
+    var input = (MalString) args.get(0);
+    var regex = (MalString) args.get(1);
+
+    var matcher = Pattern.compile(regex.value()).matcher(input.value());
+
+    var fullResult = matcher.results().map(result -> {
+      var list = new ArrayList<MalNode>();
+      list.add(string(result.group(0)));
+      for (int i = 1; i <= result.groupCount(); i++) {
+        var group = result.group(i);
+        if (group != null) {
+          list.add(string(group));
+        } else {
+          list.add(NIL);
+        }
+      }
+      return list(list);
+    }).toList();
+
+    return done(list(fullResult));
+  };
+
   MalLambda TO_NUMBER = args -> {
     var input = (MalString) args.get(0);
     return done(number(Integer.parseInt(input.value())));
@@ -39,6 +66,7 @@ public interface Strings {
   Map<String, MalNode> NS = Map.ofEntries(
     entry("split-lines", function(SPLIT_LINES)),
     entry("split", function(SPLIT)),
-    entry("to-number", function(TO_NUMBER))
+    entry("to-number", function(TO_NUMBER)),
+    entry("find-matches", function(FIND_MATCHES))
   );
 }
