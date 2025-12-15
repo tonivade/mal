0872fe00b30ba9e762bc7cdb972bf64a64d7d024
Antonio Muñoz <antonio.munoz@clarity.ai>
2025-12-15 11:57:37 +0100

    fix readline

--- impls/java25/src/main/java/mal/Readline.java
+++ impls/java25/src/main/java/mal/Readline.java
@@ -28,7 +28,7 @@ class Readline {
       "vector?", "sequential?", "map?", "contains?", "symbol", "keyword", "vector", "get",
       "keys", "vals", "hash-map", "assoc", "dissoc", "readline", "time-ms", "meta", "with-meta",
       "fn?", "string?", "number?", "seq", "conj", "def!", "defmacro!", "let*", "do", "try*",
-      "if", "fn", "quote", "quasiquote", "catch*", "eval", "require"))
+      "if", "fn", "quote", "quasiquote", "catch*", "eval", "import", "java-eval"))
     .highlighter(new DefaultHighlighter())
     .parser(new DefaultParser().lineCommentDelims(new String[] { ";" }))
     .build();
